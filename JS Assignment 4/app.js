70c80167dc7569cb7938653972a2d4c9c7c2f9f2
Syed Muhammad Saad <136969885+SyedMuhammadSaad74@users.noreply.github.com>
2023-12-31 14:15:58 +0500

    Add files via upload

---
+++ JS Assignment 4/app.js
@@ -0,0 +1,46 @@
+// *************************CHAPTER (17-20)******************************
+
+// QUESTION#04
+// var table =+prompt("Enter the table no");
+// var lengthOfTable =+prompt("Enter the table of length");
+// for (var i=1; i<=lengthOfTable; i++){
+//     (table + " " + "x"+" " + " " + i + " " + "=" + table*i)
+// }
+
+// QUESTION#05
+// var fruits = ["Apple","Banana","Mango","Orange", "Strawberry"]
+// for(var i=0 ; i <=fruits.length-1 ; i++)
+// console.log("Element at"+ " " +i + " " +"index is"+ " " +fruits[i]);
+
+// QUESTION#06
+
+//COUNTING:
+// for (var i=0; i<=20 ; i++){
+// console.log(i);
+// }
+
+//REVERSING COUNTING:
+// for (var i=10; i>0 ; i--){
+//     console.log(i);
+// }
+
+// EVEN NUMBERS:
+// for (var i=0; i<=20 ; i++){
+//     if(i%2 ==0){
+//         console.log(i);        
+//     }
+// }
+
+//ODD NUMBERS:
+// for (var i=0; i<=20 ; i++){
+//     if(i%2 ==1){
+//         console.log(i);        
+//     }
+// }
+
+//SERIES:
+// for (var i=0; i<=20 ; i++){
+//     if(i%2 ==0){
+//         console.log(i+"k");        
+//     }
+// }
